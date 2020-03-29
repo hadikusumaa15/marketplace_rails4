f93734edc369c0fa48ab0a00481bffa3a7e36538
hadikusumaa15 <hadikusumaa15@gmail.com>
2020-03-29 18:43:45 +0700

    add config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
-require 'api_constraints'
-
 Rails.application.routes.draw do
-  mount SabisuRails::Engine => "/sabisu_rails"
-
-  devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
-      resources :users, :only => [:show]
-    end
+    # We are going to list our resources here
   end
 end
