37e08f1c32079483025e650c038933a319179ed1
hadikusumaa15 <hadikusumaa15@gmail.com>
2020-03-31 08:36:02 +0700

    Adds the authenticate with token method to handle access to actions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -3,4 +3,10 @@ module Authenticable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
+
+  # we don't need to write this in newer version of devise :
+  def authenticate_with_token!
+    render json: { errors: "Not authenticated" },
+                status: :unauthorized unless current_user.present?
+  end
 end
