a8b888b522955e0a9df2248d637594f3b663b045
hadikusumaa15 <hadikusumaa15@gmail.com>
2020-03-31 08:21:23 +0700

    Adds destroy session action added

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
 describe Api::V1::SessionsController do
-
   describe "POST #create" do
-
-   before(:each) do
-    @user = FactoryGirl.create :user
-   end
+    before(:each) do
+      @user = FactoryGirl.create :user
+    end
 
     context "when the credentials are correct" do
-
       before(:each) do
         credentials = { email: @user.email, password: "12345678" }
         post :create, { session: credentials }
@@ -24,7 +21,6 @@ describe Api::V1::SessionsController do
     end
 
     context "when the credentials are incorrect" do
-
       before(:each) do
         credentials = { email: @user.email, password: "invalidpassword" }
         post :create, { session: credentials }
@@ -36,6 +32,15 @@ describe Api::V1::SessionsController do
 
       it { should respond_with 422 }
     end
+  end
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, id: @user.auth_token
+    end
+
+    it { should respond_with 204 }
   end
 end
 # brspec spec/controllers/api/v1/sessions_controller_spec.rb
