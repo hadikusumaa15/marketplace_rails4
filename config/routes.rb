6f84c013d7f7a0740b9b3771fc7ea49fb05ca66d
hadikusumaa15 <hadikusumaa15@gmail.com>
2020-04-06 03:07:38 +0700

    Adds index orders controller action nested into the users resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
                               constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show, :create, :update, :destroy]
-      resources :sessions, :only => [:create, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy] do
+        resources :products, :only => [:create, :update, :destroy]
+        resources :orders, :only => [:index]
+      end
       resources :products, :only => [:show, :index]
+      resources :sessions, :only => [:create, :destroy]
     end
   end
 end
